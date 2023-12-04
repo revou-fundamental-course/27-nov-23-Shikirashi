cd82b63e8138566acfe0a4a7de03c7e3c59721f0
Shikirashi <kirinokantai@yahoo.com>
2023-12-04 19:29:41 +0700

    added reset button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 function LuasSegitiga() {
   var a = document.getElementById("alas").value;
   var t = document.getElementById("tinggi").value;
@@ -9,6 +7,9 @@ function LuasSegitiga() {
   }
   var hasil = 0.5 * a * t;
   document.getElementById("hasil").innerHTML = "Hasil: luas segitiga adalah " + hasil + "cm" + '<sup>2</sup>';
-  console.log(hasil);
-  console.log("Test");
+}
+
+function ResetForm(){
+  document.getElementById("input-form").reset();
+  document.getElementById("hasil").innerHTML = "Hasil:";
 }
